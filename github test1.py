8ab75e457d5fa025afe8e868f6e2f498cf345e4d
Changhwan Park <pcwmo99@naver.com>
2024-03-14 15:51:41 +0900

    third commit

--- github test1.py
+++ github test1.py
@@ -1,2 +1,46 @@
+print(1234)
 print("hello world")
-print("zlzlzlzlzlzlzzl")
+print('1234')
+print(True)
+print(False)
+
+envelope1=10000
+print(envelope1)
+print(5==2)
+print(5!=2)
+print('c'in'cat')
+print('c'not in 'cat')
+int('2')
+float('2.5')
+str(1)
+bool('  ')
+bool('')
+#이건 주석이다
+'''이것도 주석이야 주석
+주석 주석 주석 주석'''
+
+lang='python'
+print(lang[0])
+print(lang[1:6])
+print(lang[:])
+print(lang)
+
+snack="꿀꽈배기"
+two='2개'
+
+juseyo=snack+two
+
+print(juseyo)
+
+juseyo+='주세요'
+
+print(juseyo)
+
+print(len(juseyo))
+
+snack='''꿀꽈배기는
+너무
+맛있어요'''
+
+print(snack)
+
